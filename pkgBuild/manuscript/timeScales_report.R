30f3d007aeb27b83e5be4422ee9d8354211b69d1
rBatt <battrd@gmail.com>
2017-09-21 17:10:03 -0400

    put ac1 function into own file

--- pkgBuild/manuscript/timeScales_report.R
+++ pkgBuild/manuscript/timeScales_report.R
@@ -109,37 +109,6 @@ detrend <- function(x, time=1:length(x)){
 	stats::residuals(stats::lm(x~time))
 }
 
-# AC1
-# Calculate first-order autocorrelation
-#
-# @param x numeric vector of values of a regular (equally-spaced) time series
-#
-# @return AR(1) coefficient
-# @export
-ac1 <- function(x, ...){	
-	# # option 1
-	l2 <- embed(x, 2)
-	ac <- cor(l2[,1], l2[,2], use="na.or.complete")
-	#
-	# # option 2
-	# ac <- ar(x, order.max=1)$ar # returns numeric(0) if nothing fit
-	#
-	# # option 3
-	# l2 <- embed(x, 2)
-	# ac <- lm(I(l2[,1]) ~ I(l2[,2]))$coef[2]
-	
-	# option 4
-	# detX <- detrend(x)
-	# if(all(is.na(x))){return(NA)}
-#	# l2 <- stats::embed(x, 2)
-#	# ac <- tryCatch(stats::lm(I(l2[,1]) ~ I(l2[,2]))$coef[2], error=function(cond)NA)
-	
-	# # option 4
-	# l2 <- embed(x, 2)
-	# out <- RollingWindow::RollingCorr(l2[,2], l2[,1], window=win)
-	
-	return(ac)
-}
 
 ac_sub <- function(x, n, phase, ...){
 	l2 <- embed(x, 2)
