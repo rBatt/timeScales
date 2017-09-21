7a67cc54dcca85f5fe00d1c0ffad0f49dab774f6
rBatt <battrd@gmail.com>
2017-09-21 17:20:10 -0400

    put ac_sub into ac1.R file, add doc

--- R/ac1.R
+++ R/ac1.R
@@ -29,3 +29,26 @@ ac1 <- function(x, ...){
 	
 	return(ac)
 }
+
+
+#' Subset AC1
+#' 
+#' Calculate first-order autocorrelation from a subset
+#' 
+#' @param x numeric vector, time series
+#' @param n integer, subset to every n-th observation (every n-th pair of observations)
+#' @param phase integer, initial observation (pair) to use when subsetting, passed to \code{\link{sub_samp}}
+#' @param ... unused, here for compatability with other functions that might pass arguments by default
+#' 
+#' To do a subset for autocorrelation, the key is retain the adjacency of observations. For \code{n=2}, for example, we couldn't use every other observation in \code{x}, as this would fundamentally change the time scale of the calculation. Thus, we still want to calculate the correlation between adjacent samples. So in this example, we would use every other adjacent pair. E.g., samples 1-2, (skip 2-3), 3-4, (skip 4-5), ... and so on.
+#' 
+#' @return AR1 coefficient
+#' @export
+ac_sub <- function(x, n, phase, ...){
+	l2 <- embed(x, 2)
+	row_vec <- seq_len(nrow(l2))
+	row_ind <- sub_samp(row_vec, n=n, phase=phase)
+	l2_sub <- l2[row_ind, ]
+	ac <- cor(l2_sub[,1], l2_sub[,2], use="na.or.complete")
+	return(ac)
+}
--- pkgBuild/manuscript/timeScales_report.R
+++ pkgBuild/manuscript/timeScales_report.R
@@ -110,16 +110,6 @@ detrend <- function(x, time=1:length(x)){
 }
 
 
-ac_sub <- function(x, n, phase, ...){
-	l2 <- embed(x, 2)
-	row_vec <- seq_len(nrow(l2))
-	row_ind <- sub_samp(row_vec, n=n, phase=phase)
-	l2_sub <- l2[row_ind, ]
-	ac <- cor(l2_sub[,1], l2_sub[,2], use="na.or.complete")
-	return(ac)
-}
-
-
 #' ##Rolling Windows and Aggregation
 #+ functions-windows-agg
 roundGrid <- function(x, frac=1){
