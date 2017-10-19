45451366ae3838fded968fd9ea932e7d543cf3dd
rBatt <battrd@gmail.com>
2017-10-19 11:48:22 -0400

    change default maximum order of polynomial to 6

--- R/acf_roll.R
+++ R/acf_roll.R
@@ -34,7 +34,7 @@ acf_roll <- function(x, width=28*24, by=12, lag.max=width/14, DETREND=FALSE, ...
 	if(DETREND){
 		detrend2 <- function(z){
 			z <- stats::ts(z, frequency=stats::frequency(x))
-			as.numeric(detrendR(z, max_poly=4, max_fourier=6, max_interaction=3))
+			as.numeric(detrendR(z, max_poly=6, max_fourier=6, max_interaction=3))
 		}
 		mat <- t(apply(X=mat, MARGIN=1, FUN=detrend2))
 	}
