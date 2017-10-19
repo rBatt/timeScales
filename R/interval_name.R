f7dbdbb5d5aacc793fcb1bb5667477f0318bef09
rBatt <battrd@gmail.com>
2017-10-19 18:42:04 -0400

    accept sampling freqs other than 5 min
    accept other time scales

--- R/interval_name.R
+++ R/interval_name.R
@@ -3,6 +3,7 @@
 #' Converts a time interval (integer number of observations) into words, assuming 5 minute data
 #' 
 #' @param x integer, the number of time steps
+#' @param minPerSample time elapsed between samples, in minutes
 #' 
 #' @examples
 #' interval_name(5)
@@ -10,17 +11,17 @@
 #' interval_name(288*1.25)
 #' 
 #' @export
-interval_name <- function(x){
+interval_name <- function(x, minPerSample=5){
 	# assumes 5 minute data
-	interv <- x*5
+	interv <- x*minPerSample
 	if(interv < 60){
-		unit <- "min"
+		unit <- "m"
 		val <- interv
 	}else if(interv >= 60 & interv < 1440){
-		unit <- "hr"
+		unit <- "h"
 		val <- round(interv/60, 2)
 	}else{
-		unit <- "day"
+		unit <- "d"
 		val <- round(interv/60/24, 2)
 	}
 	iname <- paste(val, unit, sep="-")
