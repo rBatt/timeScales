539e24db9f759cc26537d7ecf97b6b09c8a881cf
rBatt <battrd@gmail.com>
2018-03-20 15:33:52 -0400

    fix and improve lake P simulation documentation

--- R/simLakeP.R
+++ R/simLakeP.R
@@ -9,15 +9,17 @@
 # ===================================
 #' Eutrophication Bifurcation
 #' Model of lake eutrophication via elevated phosphorus loading. When as phosphorus loading increases, a fold bifurcation is reached, and the system switches into a eutrophic state from a oligotrophic state. 
+#' @param state state variables, a vector of length 3 with names "X", "M", and "U"
+#' @param pars a named vector of parameters; currently only option is "C", a coefficient controlling the fraction of soil P that washes into the lake (units of 'per year'); set up this way so as to be compatible with functions in package "rootSolve" (C is a control parameter affecting water quality).
 #' @param F input rate of phosporus to soil
 #' @param C coefficient for transfer of soil phosphorus to the lake
-#' @param H noise for input to the lake
+# @param H noise for input to the lake
 #' @param s sedimentation loss
 #' @param h hydrologic loss (outflow)
 #' @param r recycling coefficient
 #' @param sigma standard deviation of recycling noise
 #' @param b permanent burial rate of phosphorus in sediments
-#' @param dW a white noise process with mean zero and variance dt
+# @param dW a white noise process with mean zero and variance dt
 #' @param m in the recycling function, the value of X at which recycling is half the maximum rate
 #' @param q in the recycling function, the exponent q determines the slope of R(X) near m
 
@@ -54,7 +56,7 @@
 # =======================================
 # = Model for 1 Deterministic Time Step =
 # =======================================
-modelDeterministic <- function(state, pars=c(C=0.00115), F=14.6, b=0.001, h=0.15, m=2.4, r=0.019, q=8, s=0.7, sigma=0.01, lambda=0.35){
+modelDeterministic <- function(state, pars=c(C=0.00115), F=14.6, b=0.001, h=0.15, m=2.4, r=0.019, q=8, s=0.7, sigma=0.01){
 	with(as.list(c(state, pars)),{ # allows referring to names as variables; convenient
 		R <- (X^q)/(m^q + X^q) # recycling
 		H <- 1 # set to 1 as per appendix
@@ -141,7 +143,7 @@ getRoot <- function(x){
 
 #' Get the Root of the Eutrophication Model from a Data Frame
 #' Finds the roots of the eutrophication model given starting values (water, mud P) and parameter (P loading)
-#' @param initalValues a \code{data.frame} with 3 columns named X (water P), M (mud P), and I (P loading)
+#' @param initialValues a \code{data.frame} with 3 columns named X (water P), M (mud P), and I (P loading)
 #' @return a matrix with columns for initial values of X and M, roots (of X and M), and the bifurcation parameter I. Each row of output corresponds to a row of the input (though output reorded to ascending I). Original columns for state variables (X and M) will be renamed to "init.X" and "init.M". The equilibria that would be approached from these initial values, i.e. the 'roots', will take on the column names of "X" and "M". Thus, the columns X & M will not have the same values in the input as in the output (output are equilibrium values), unless the input states were already at equilibrium.
 #' @export
 getRoot_df <- function(initialValues){
@@ -162,7 +164,8 @@ getEigs <- function(x){
 }
 #' Get Eigenvalues for Eutrophication Model (from data frame of states)
 #' Given the rate of change of state variables (water and mud P) and the input P, returns the eigenvalues
-#' @param x a matrix with columns named for X, M, I, as in \code{\link{modelDeterministicXM}}
+#' @param rootGrid_I a matrix with columns named for X, M, I, as in \code{\link{modelDeterministicXM}}; these columns are the equilibria (roots) of the state variables, and I, the P input parameter.
+#' @note the "Grid" name just comes from legacy coding where I was trying a "grid" (as in, e.g., \code{expand.grid}) of X, M, and I values as initial values for which I was calculating equilibria.
 #' @seealso \code{\link{getEigs}}
 #' @return data.frame of eigenvalues
 #' @export
