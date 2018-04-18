3a4085e25c82aa9a52ba7ecaa8f5abae56804666
rBatt <battrd@gmail.com>
2018-04-18 19:27:11 -0400

    fix naming in package data
    lake names fixed ... but also i had at one point accidentally duplicated data points through an error in my code to rename lakes

--- pkgBuild/sos_data.R
+++ pkgBuild/sos_data.R
@@ -9,4 +9,6 @@ setwd("~/Documents/School&Work/epaPost/timeScales/pkgBuild")
 load("../inst/extdata/LRT_Squeal2_allYears_HighFreq_SondeData_MARSSinterps_v2.Rdata")
 dat <- as.data.table(LRT_Squeal2_allYears_HighFreq_SondeData_MARSSinterps_v2)
 sos_data <- dat[,list(Year, Lake, DoY, Temp_HYLB, Chl_HYLB, Chl_logged_HYLB, BGA_HYLB, BGA_logged_HYLB)]
+sos_data[,Lake:=list(c("L"="Paul", "R"="Peter", "T"="Tuesday")[Lake])]
+setorder(sos_data, Year, Lake, DoY)
 save(sos_data, file="../data/sos_data.RData", compress='xz')
