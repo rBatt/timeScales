d043c37a602b0a3e1a651fd23e695ffffde134b1
rBatt <battrd@gmail.com>
2018-04-18 16:47:37 -0400

    use MARSS interpolated data

--- pkgBuild/sos_data.R
+++ pkgBuild/sos_data.R
@@ -2,8 +2,11 @@ library("data.table")
 
 setwd("~/Documents/School&Work/epaPost/timeScales/pkgBuild")
 
-dat <- fread("../inst/extdata/Daily_SoS_All_Orgd&Cleaned_Data_2015.csv")
+# dat <- fread("../inst/extdata/Daily_SoS_All_Orgd&Cleaned_Data_2015.csv")
+# sos_data <- dat[,list(Year, Lake, DoY, DateTime, Temp_HYLB, Chla_Conc_HYLB, BGA_Conc_HYLB)]
+# save(sos_data, file="../data/sos_data.RData")
 
-sos_data <- dat[,list(Year, Lake, DoY, DateTime, Temp_HYLB, Chla_Conc_HYLB, BGA_Conc_HYLB)]
-
-save(sos_data, file="../data/sos_data.RData")
+load("../inst/extdata/LRT_Squeal2_allYears_HighFreq_SondeData_MARSSinterps_v2.Rdata")
+dat <- as.data.table(LRT_Squeal2_allYears_HighFreq_SondeData_MARSSinterps_v2)
+sos_data <- dat[,list(Year, Lake, DoY, Temp_HYLB, Chl_HYLB, Chl_logged_HYLB, BGA_HYLB, BGA_logged_HYLB)]
+save(sos_data, file="../data/sos_data.RData", compress='xz')
